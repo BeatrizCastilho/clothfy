90aab62891b48020067b4559579322e899386c13
BeatrizCastilho <beatriz.c.castilho@aluno.senai.br>
2026-06-12 08:22:37 -0300

    Update catalogo.js

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,30 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const produtosCards = document.querySelectorAll(".produto");
+const botoes = document.querySelectorAll("button");
 
-    produtosCards.forEach(card => {
-        const btn = card.querySelector("button");
-        const titulo = card.querySelector("h3").textContent;
-        const precoTexto = card.querySelector("p").textContent;
-        const imagemSrc = card.querySelector("img").getAttribute("src");
+botoes.forEach(botao => {
 
-        // Tratamento simples para converter o texto "R$ 149,90" em número real float (149.90)
-        const precoNumerico = parseFloat(precoTexto.replace("R$", "").replace(",", ".").trim());
+botao.addEventListener("click", () => {
 
-        // Altera o comportamento do botão para salvar o item do catálogo diretamente
-        btn.textContent = "Comprar / Customizar";
-        btn.addEventListener("click", () => {
-            let carrinho = JSON.parse(localStorage.getItem("clothfyCart")) || [];
-            
-            carrinho.push({
-                id: Date.now(),
-                nome: titulo,
-                preco: precoNumerico,
-                imagemMock: imagemSrc
-            });
+const produto =
+botao.parentElement.querySelector("h3").innerText;
 
-            localStorage.setItem("clothfyCart", JSON.stringify(carrinho));
-            alert(`🛍️ "${titulo}" foi adicionado ao seu carrinho! Redirecionando para finalização.`);
-            window.location.href = "carrinho.html";
-        });
-    });
+window.location.href =
+`personalizar.html?produto=${encodeURIComponent(produto)}`;
+
+});
+
 });
